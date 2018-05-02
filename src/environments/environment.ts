fe7e7e131181480c4bc732be8be3366e435ccc3b
Vinh Pham <vinhpz@outlook.com>
2018-05-02 16:43:16 +0700

    Update environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -17,5 +17,5 @@ export const CONFIG: IConfig = {
     storageBucket: 'ntrivia-86e25.appspot.com',
     messagingSenderId: '579311413632'
   },
-  'functionsUrl': 'https://us-central1-rwa-trivia-dev-e57fc.cloudfunctions.net/'
+  'functionsUrl': 'https://us-central1-ntrivia-86e25.cloudfunctions.net/'
 };
